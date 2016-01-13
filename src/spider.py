a4ed05bb8eb1da02ff6783324017d17941eea123
skywod <skywod@shnow.cn>
2016-01-13 11:50:14 +0800

    天气接口切换为和风天气API

--- src/spider.py
+++ src/spider.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
-import urllib,sys,re,time
+import urllib,sys,re,time,json
 from datetime import date
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 city_code = '101020100'
 proxies = {'http': 'http://web-proxy.oa.com:8080'}
@@ -11,24 +13,26 @@ try_cnt = 0
 
 while has_error == True and try_cnt < 10:
 	try:
-		url = 'http://www.weather.com.cn/weather1d/%s.shtml' % city_code
+		url = 'https://api.heweather.com/x3/weather?cityid=CN%s&key=f02205ac528a4e7d986c4ad8ca4481c3' % city_code
 		#filehandle = urllib.urlopen(url, proxies=proxies)
 		filehandle = urllib.urlopen(url)
 		content = filehandle.read()
-		pt = re.compile(r'<p class="wea" title="(.*?)">.*?</p>',re.S)
-		match = pt.findall(content)
-		#print match
-		day = match[0]
-		night = match[1]
+		weather = json.loads(content)
+		forecasts = weather['HeWeather data service 3.0'][0]['daily_forecast']
+		
+		for forecast in forecasts:
+			if forecast['date'] == str(date.today()):
+                		day = forecast['cond']['txt_d']
+                		night = forecast['cond']['txt_n']
+				day_temperature = forecast['tmp']['max']
+                		night_temperature = forecast['tmp']['min']
+               		 	print ('day:%s day_temperature:%s night:%s,night_temperature:%s'%(day, day_temperature, night, night_temperature))
+                		fp = open('%s/data/%s.weather'%(sys.path[0],city_code),'a+')
+                		fp.write('date:%s day:%s day_temperature:%s night:%s,night_temperature:%s\r\n'%(date.today() ,day, day_temperature, night, night_temperature))
+                		fp.close()
 
-		pt = re.compile(r'<span>(\d+)</span><em>°C</em>',re.S)
-		match = pt.findall(content)
-		day_temperature = match[0]
-		night_temperature = match[1]
-		print ('day:%s day_temperature:%s night:%s,night_temperature:%s'%(day, day_temperature, night, night_temperature))
-		fp = open('%s/data/%s.weather'%(sys.path[0],city_code),'a+')
-		fp.write('date:%s day:%s day_temperature:%s night:%s,night_temperature:%s\r\n'%(date.today() ,day, day_temperature, night, night_temperature))
-		fp.close()
+			else:
+				break
 		has_error = False
 	except Exception , e:
 		print e
